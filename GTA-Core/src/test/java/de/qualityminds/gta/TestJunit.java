32a8e4292df0d00d20afcf1172b9bb1741126715
Johann Förster <johann.foerster@qualityminds.de>
2019-10-24 15:57:35 +0200

    Add @RunWith annotation to JUnit Test for property dependency injection

--- GTA-Core/src/test/java/de/qualityminds/gta/TestJunit.java
+++ GTA-Core/src/test/java/de/qualityminds/gta/TestJunit.java
@@ -3,12 +3,15 @@ package de.qualityminds.gta;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import de.qualityminds.gta.config.TestProperties;
 import de.qualityminds.gta.spring.SpringConfig;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringConfig.class)	
 public class TestJunit {	
 	@Autowired
