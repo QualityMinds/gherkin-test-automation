4a783af4654e87953e54f0e843ea8ed3ac249693
Tobias Varlemann <tobias.varlemann@qualityminds.de>
2019-11-25 21:00:20 +0100

    Add push to sonaType nexus

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -3,7 +3,8 @@ val serenityCucumberVersion = "1.0.21"
 val springBootVersion = "2.2.0.RELEASE"
 
 plugins {
-    id("java-library")
+    `java-library`
+    `maven-publish`
     // id("net.serenity-bdd.aggregator") version "2.0.81"
 }
 
@@ -30,4 +31,63 @@ tasks.named<Test>("test") {
     systemProperty("file.encoding", "utf-8")
 }
 
-gradle.startParameter.setContinueOnFailure(true)                 
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("source")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            val releasesRepoUrl = uri("https://oss.sonatype.org/content/repositories/releases/")
+            credentials(PasswordCredentials::class.java) {
+                username = rootProject.extra.get("ossrhUser")?.toString()
+                password = rootProject.extra.get("ossrhPassword")?.toString()
+            }
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            pom {
+                name.set("GTA Core")
+                description.set("The core module of GTA")
+                url.set("https://github.com/QualityMinds/gherkin-test-automation/tree/master/Core")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("tva")
+                        name.set("Tobias Varlemann")
+                        email.set("tobias.varlemann@qualityminds.de")
+                    }
+                    developer {
+                        id.set("jfo")
+                        name.set("Johann Förster")
+                        email.set("johann.foerster@qualityminds.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/QualityMinds/gherkin-test-automation.git")
+                    developerConnection.set("scm:git:https://github.com/QualityMinds/gherkin-test-automation.git")
+                    url.set("https://github.com/QualityMinds/gherkin-test-automation/tree/master/Core")
+                }
+            }
+
+        }
+    }
+}
+
+gradle.startParameter.setContinueOnFailure(true)
