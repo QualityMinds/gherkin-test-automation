7a65f6b8731bb594e535941a94dc5062385ee87d
Tobias Varlemann <tobias.varlemann@qualityminds.de>
2020-01-31 11:09:22 +0100

    Fix Spring problems in Core module

--- Core/src/main/java/de/qualityminds/gta/SpringConfig.java
+++ Core/src/main/java/de/qualityminds/gta/SpringConfig.java
@@ -1,6 +1,6 @@
-package config;
+package de.qualityminds.gta;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("de.qualityminds.rd.bdd")
+@ComponentScan("de.qualityminds.gta")
 public class SpringConfig {}
